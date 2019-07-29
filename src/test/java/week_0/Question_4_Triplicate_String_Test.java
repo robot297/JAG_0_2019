0d6722e45561e6e3b5cd5a8e6fa2828bb0b8dde5
clara <clara@minneapolis.edu>
2019-07-28 19:17:15 -0500

    Tidy test

--- src/test/java/week_0/Question_4_Triplicate_String_Test.java
+++ src/test/java/week_0/Question_4_Triplicate_String_Test.java
@@ -2,6 +2,8 @@ package week_0;
 
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.junit.Assert.*;
 import static week_0.Question_4_Triplicate_String.triplicate;
 
@@ -11,17 +13,18 @@ public class Question_4_Triplicate_String_Test {
     @Test(timeout=3000)
     public void tripleString() {
         
-        String[] in = { "cat", "CAT", "Java", "Hello World!"};
-        String[] out = {"catcatcat", "CATCATCAT", "JavaJavaJava", "Hello World!Hello World!Hello World!"};
+        Map<String, String> inOut = Map.of(
+                "cat", "catcatcat",
+                "CAT", "CATCATCAT",
+                "Java", "JavaJavaJava",
+                "Hello World!", "Hello World!Hello World!Hello World!"
+        );
     
-        for (int s = 0; s < in.length ; s++) {
-        
-            String inSt = in[s];
-            String outSt = out[s];
-        
-            assertEquals( String.format("When calling triplicate() method with '%s', it should return '%s'", inSt, outSt), outSt, triplicate(inSt));
-        
-        }
+        inOut.forEach((inStr, expectedOutStr) -> {
+            String outStr = triplicate(inStr);
+            String message = String.format("When calling triplicate() method with '%s', it should return '%s'", inStr, expectedOutStr);
+            assertEquals(message, expectedOutStr, outStr);
+        });
     
     }
     
